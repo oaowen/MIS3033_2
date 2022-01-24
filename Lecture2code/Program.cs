29631f3a4c670b4f0178bb3e662f6dd49b07ac21
Olivia Owen <Olivia.A.Owen-1@ou.edu>
2022-01-24 10:59:54 -0600

    Add project files.

---
+++ Lecture2code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lecture2code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to MIS 3033!");
+
+            Console.ReadKey();
+        }
+    }
+}
